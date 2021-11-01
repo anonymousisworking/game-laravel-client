e2a84f2aa3aa481683ad6a96aba3820f89b7f353
homeleonn <wirus@ukr.net>
2021-11-02 01:26:47 +0200

    added migrations for clans and clan tendencies, seeds data

---
+++ database/migrations/2021_11_01_222249_create_clans_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clans', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('tendency');
+            $table->string('img');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clans');
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,25 +22,37 @@ class DatabaseSeeder extends Seeder
 
     	// Add Admin user
         DB::table('users')->insert([
-			    [
-			    	'login' => 'Admin', 
-			   		'email' => 'admin@game.test', 
-			   		'password' => '$2y$10$O8h8IC79WyN0cHQUHmeGlujPiDe.26U3hxGhnRngmMZ2PeIIxfcFe', 
-			   		'sex' => 0,
-			   		'super_hits' => generateSuperHit(),
-			   		'access_level' => 1,
-			   	],
-				]);
+    	    [
+    	    	'login' => 'Admin',
+    	   		'email' => 'admin@game.test',
+    	   		'password' => '$2y$10$O8h8IC79WyN0cHQUHmeGlujPiDe.26U3hxGhnRngmMZ2PeIIxfcFe',
+    	   		'sex' => 0,
+                'clan' => 1,
+    	   		'super_hits' => generateSuperHit(),
+    	   		'access_level' => 1,
+    	   	],
+		]);
 
-				DB::table('users')->insert([
-			    [
-			    	'login' => 'Tester', 
-			   		'email' => 'tester@game.test', 
-			   		'password' => '$2y$10$O8h8IC79WyN0cHQUHmeGlujPiDe.26U3hxGhnRngmMZ2PeIIxfcFe', 
-			   		'sex' => 1,
-			   		'super_hits' => generateSuperHit(),
-			   	],
-				]);
+		DB::table('users')->insert([
+    	    [
+    	    	'login' => 'Tester',
+    	   		'email' => 'tester@game.test',
+    	   		'password' => '$2y$10$O8h8IC79WyN0cHQUHmeGlujPiDe.26U3hxGhnRngmMZ2PeIIxfcFe',
+    	   		'sex' => 1,
+    	   		'super_hits' => generateSuperHit(),
+    	   	],
+		]);
+
+        DB::table('clans')->insert([
+            'name' => 'Developers',
+            'tendency' => 1,
+            'img' => 'developers.png'
+        ]);
+
+        DB::table('tendencies')->insert([
+            'name' => 'Neutral',
+            'img' => 'neutral.gif',
+        ]);
 
 
         // Add Locations
@@ -49,7 +61,7 @@ class DatabaseSeeder extends Seeder
 		    	'id' => 1,
 		    	'name' => 'Торговая площадь',
 		    	'type' => 'location',
-		    	'image' => 'area.jpg', 
+		    	'image' => 'area.jpg',
 		    	'loc_coords' => '
 					{"2": "169,152 169,149 167,145 165,134 168,127 172,125 173,103 168,100 169,94 178,91 178,62 183,57 188,61 188,65 202,61 205,53 217,51 221,63 233,69 240,64 244,68 243,80 240,90 245,93 245,98 242,104 244,112 244,122 240,127 284,124 283,118 292,106 302,116 305,123 314,124 314,101 320,96 324,102 325,118 332,112 339,120 343,126 340,142 343,158 336,159 335,152 301,153 289,165 224,162 220,155 210,153 202,161 182,162 177,155",
 					"3": "169,192 182,184 185,168 177,154 140,154 125,173 124,182 163,196",
@@ -64,7 +76,7 @@ class DatabaseSeeder extends Seeder
 		    	'id' => 2,
 		    	'name' => 'Башня сражений',
 		    	'type' => 'location',
-		    	'image' => 'battle-tower.jpg', 
+		    	'image' => 'battle-tower.jpg',
 		    	'loc_coords' => null,
 					'loc_access' => '[1]'
 		    ],
@@ -72,7 +84,7 @@ class DatabaseSeeder extends Seeder
 		    	'id' => 3,
 		    	'name' => 'Магазин',
 		    	'type' => 'object',
-		    	'image' => null, 
+		    	'image' => null,
 		    	'loc_coords' => null,
 					'loc_access' => '[1]'
 		    ],
@@ -80,7 +92,7 @@ class DatabaseSeeder extends Seeder
 		    	'id' => 4,
 		    	'name' => 'Банк',
 		    	'type' => 'object',
-		    	'image' => null, 
+		    	'image' => null,
 		    	'loc_coords' => null,
 					'loc_access' => '[1]'
 		    ],
@@ -88,7 +100,7 @@ class DatabaseSeeder extends Seeder
 		    	'id' => 5,
 		    	'name' => 'Фонтан',
 		    	'type' => 'character',
-		    	'image' => null, 
+		    	'image' => null,
 		    	'loc_coords' => null,
 					'loc_access' => '[1]'
 		    ],
@@ -96,7 +108,7 @@ class DatabaseSeeder extends Seeder
 		    	'id' => 6,
 		    	'name' => 'Озеро',
 		    	'type' => 'location',
-		    	'image' => 'lake.jpeg', 
+		    	'image' => 'lake.jpeg',
 		    	'loc_coords' => '{
 					"1": "169,192 182,184 185,168 177,154 140,154 125,173 124,182 163,196"
 				}',
@@ -106,7 +118,7 @@ class DatabaseSeeder extends Seeder
 		    	'id' => 7,
 		    	'name' => 'Лес',
 		    	'type' => 'location',
-		    	'image' => 'forest.jpg', 
+		    	'image' => 'forest.jpg',
 		    	'loc_coords' => null,
 					'loc_access' => '[1]'
 		    ],
@@ -340,7 +352,7 @@ class DatabaseSeeder extends Seeder
 
         DB::table('npc')->insert([
             [
-                'name' => 'Ящер', 
+                'name' => 'Ящер',
                 'level' => 0,
                 'sex' => 0,
                 'curhp' => 18,
@@ -353,17 +365,17 @@ class DatabaseSeeder extends Seeder
             ]
         ]);
 
-        
+
 
         DB::table('spawnlist')->insert([
             [
-                'npc_id' => 1, 
+                'npc_id' => 1,
                 'loc_id' => 1,
             ],[
-                'npc_id' => 1, 
+                'npc_id' => 1,
                 'loc_id' => 1,
             ],[
-                'npc_id' => 1, 
+                'npc_id' => 1,
                 'loc_id' => 6,
             ],
         ]);
